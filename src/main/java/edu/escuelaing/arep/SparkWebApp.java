73e4cdd075fbc0d529d8664590484ca63c58ab07
Luis Daniel Benavides Navarro <dnielben@gmail.com>
2020-05-14 14:58:12 -0500

    Update SparkWebApp.java

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -14,7 +14,7 @@ public class SparkWebApp {
     }
 
     static int getPort() {
-        if (System.getenv("PORT") != null) {
+        if (System.getenv("PORT) != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)    }
